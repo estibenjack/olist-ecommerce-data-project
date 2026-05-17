443420c2f92c3a4cf4ee1621a435225fdd983d56
Steven <156148848+estibenjack@users.noreply.github.com>
2026-05-17 23:01:16 +0100

    added estimated delivery column

--- sql_scripts/03_staging/staging_orders.sql
+++ sql_scripts/03_staging/staging_orders.sql
@@ -9,6 +9,7 @@ SELECT
 	CAST(order_approved_at AS TIMESTAMP) AS order_approved_at,
 	CAST(order_delivered_carrier_date AS TIMESTAMP) AS order_delivered_carrier_date,
 	CAST(order_delivered_customer_date AS TIMESTAMP) AS order_delivered_customer_date,
+  CAST(order_estimated_delivery_date AS TIMESTAMP) AS order_estimated_delivery_date,
 	CASE
 		WHEN CAST(order_delivered_customer_date AS TIMESTAMP) > CAST(order_estimated_delivery_date AS TIMESTAMP)
 		THEN 1
