72da5f3de9e8b139c2cc22b36ccbc03b75c4bf49
Steven <156148848+estibenjack@users.noreply.github.com>
2026-05-20 18:32:31 +0100

    add customer_region column to delivery_performance

--- sql_scripts/04_gold/delivery_performance.sql
+++ sql_scripts/04_gold/delivery_performance.sql
@@ -9,6 +9,13 @@ WITH deduped_reviews AS (
 SELECT
     f.order_id,
     f.customer_state,
+    CASE
+        WHEN f.customer_state IN ('AM', 'RR', 'AP', 'PA', 'TO', 'RO', 'AC') THEN 'North'
+        WHEN f.customer_state IN ('MA', 'PI', 'CE', 'RN', 'PB', 'PE', 'AL', 'SE', 'BA') THEN 'Northeast'
+        WHEN f.customer_state IN ('MT', 'MS', 'GO', 'DF')                    THEN 'Central-West'
+        WHEN f.customer_state IN ('SP', 'RJ', 'ES', 'MG')                    THEN 'Southeast'
+        WHEN f.customer_state IN ('PR', 'SC', 'RS')                           THEN 'South'
+    END AS customer_region,
     f.seller_state,
     f.category_name_english,
     DATE_PART('day', f.order_delivered_customer_date - f.order_purchase_timestamp)        AS days_to_delivery,
@@ -17,5 +24,3 @@ SELECT
     r.review_score
 FROM gold.fact_sales f
 LEFT JOIN deduped_reviews r ON f.order_id = r.order_id;
-
-select * from gold.delivery_performance;
